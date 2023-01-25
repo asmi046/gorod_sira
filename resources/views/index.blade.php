41e771b2eec1cc5fca4dbfbc95144ca8d68f1bf5
asmi046 <asmi046@gmail.com>
2023-01-25 12:07:05 +0300

    Start

---
+++ resources/views/index.blade.php
@@ -0,0 +1,14 @@
+
+@extends('layouts.all')
+
+@php
+    $title = "Производство сыра и молочьных продуктов";
+    $description = "Производство сыра и молочьных продуктов";
+@endphp
+
+@section('title', $title)
+@section('description', $description)
+
+@section('content')
+
+@endsection
