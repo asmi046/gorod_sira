9e9f4913f2983905d2dd3a7af2e1049283bd718e
asmi046 <asmi046@gmail.com>
2023-03-06 14:19:49 +0300

    Загрузка

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -33,23 +33,23 @@ class ProductSeeder extends Seeder
                     $main_data[] =
                         [
 
-                            'sku' => trim($data[1]),
-                            'title' => trim($data[2]),
-                            'slug' => Str::slug(trim($data[2])),
+                            'sku' => trim(iconv("windows-1251", "utf-8", $data[1]))."-".$row,
+                            'title' => iconv("windows-1251", "utf-8", trim($data[2])),
+                            'slug' => Str::slug(trim(iconv("windows-1251", "utf-8", $data[2]))."-".$row),
                             'tm' => "Рыльский сыродел",
-                            'upacovka' => trim($data[4]),
-                            'quote' => trim($data[0])." - ".trim($data[2]),
+                            'upacovka' => trim(iconv("windows-1251", "utf-8", $data[4])),
+                            'quote' => trim(iconv("windows-1251", "utf-8", $data[0]))." - ".trim(iconv("windows-1251", "utf-8", $data[2])),
 
-                            'param_zgirnost' => trim($data[5]),
-                            'param_scode' => trim($data[6]),
-                            'param_ves_ed' => trim($data[7]),
-                            'param_ves_yashik' => trim($data[8]),
-                            'param_count_in_pack' => trim($data[9]),
-                            'param_srok_realiz' => trim($data[10]),
+                            'param_zgirnost' => trim(iconv("windows-1251", "utf-8", $data[5])),
+                            'param_scode' => trim(iconv("windows-1251", "utf-8", $data[6])),
+                            'param_ves_ed' => trim(iconv("windows-1251", "utf-8", $data[7])),
+                            'param_ves_yashik' => trim(iconv("windows-1251", "utf-8", $data[8])),
+                            'param_count_in_pack' => trim(iconv("windows-1251", "utf-8", $data[9])),
+                            'param_srok_realiz' => trim(iconv("windows-1251", "utf-8", $data[10])),
 
-                            'category' => trim($data[0]),
+                            'category' => trim(iconv("windows-1251", "utf-8", $data[0])),
                             'img' => trim(""),
-                            'description' => trim($data[0])." - ".trim($data[2]),
+                            'description' => trim(iconv("windows-1251", "utf-8", $data[0]))." - ".trim(iconv("windows-1251", "utf-8", $data[2])),
 
                             'price_D_ht' => floatval(trim($data[11])),
                             'price_D_kg' => floatval(trim($data[12])),
@@ -60,8 +60,8 @@ class ProductSeeder extends Seeder
                             'price_R_ht' => floatval(trim($data[15])),
                             'price_R_kg' => floatval(trim($data[16])),
 
-                            'seo_title' => trim($data[2]),
-                            'seo_description' => trim($data[0])." - ".trim($data[2]),
+                            'seo_title' => trim(iconv("windows-1251", "utf-8", $data[2])),
+                            'seo_description' => trim(iconv("windows-1251", "utf-8", $data[0]))." - ".trim(iconv("windows-1251", "utf-8", $data[2])),
 
                             // 'type' => $type,
                             // 'number' => floatval($data[0]),
@@ -80,11 +80,11 @@ class ProductSeeder extends Seeder
                             // 'home_id' => 1,
                         ];
 
-
+                        $row++;
                 }
 
                 // var_dump($main_data);
-                DB::table("kvartiras")->insert($main_data);
+                DB::table("products")->insert($main_data);
             }
     }
 }
